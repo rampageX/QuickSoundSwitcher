aba49b3d552609142320d14fcae6dcb4f2a90dbf
Odizinne <flora.aubry@proton.me>
2024-11-21 20:20:48 +0100

    added single instance lock

--- main.cpp
+++ main.cpp
@@ -3,12 +3,30 @@
 #include <QApplication>
 #include <QTranslator>
 #include <QLocale>
+#include <QProcess>
+
+bool isAnotherInstanceRunning(const QString& processName)
+{
+    QProcess process;
+    process.start("tasklist", QStringList() << "/FI" << QString("IMAGENAME eq %1").arg(processName));
+    process.waitForFinished();
+    QString output = process.readAllStandardOutput();
+    int count = output.count(processName, Qt::CaseInsensitive);
+
+    return count > 1;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setStyle("fusion");
     a.setQuitOnLastWindowClosed(false);
+
+    const QString processName = "QuickSoundSwitcher.exe";
+    if (isAnotherInstanceRunning(processName)) {
+        qDebug() << "Another instance is already running. Exiting...";
+        return 0;
+    }
 
     QString mode = Utils::getTheme();
     QString color;
