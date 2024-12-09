5eaaf30300a287da190929510065728cc246f4c5
大眼仔旭 <86864914+wcxu21@users.noreply.github.com>
2024-12-09 17:19:16 +0800

    Added simplified Chinese translation (#3)
    Added simplified Chinese translation

---
+++ Resources/Tr/QuickSoundSwitcher_zh_CN.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>OverlaySettings</name>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="55"/>
+        <source>Mute overlay settings</source>
+        <translation type="unfinished">静音叠加设置</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="299"/>
+        <source>Overlay position on screen:</source>
+        <translation type="unfinished">屏幕上的叠加位置：</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="42"/>
+        <source>Disable overlay</source>
+        <translation type="unfinished">禁用叠加</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="14"/>
+        <source>Settings</source>
+        <translation type="unfinished">设置</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="109"/>
+        <source>Volume increment percentage</source>
+        <translation type="unfinished">音量增量百分比</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="281"/>
+        <source>Use static overlay</source>
+        <translation type="unfinished">使用静态叠加</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="318"/>
+        <source>Volume control settings</source>
+        <translation type="unfinished">音量控制设置</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="338"/>
+        <source>Disable mute hotkey</source>
+        <translation type="unfinished">禁用静音热键</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="73"/>
+        <source>Disable sound notification</source>
+        <translation type="unfinished">禁用声音通知</translation>
+    </message>
+</context>
+<context>
+    <name>Panel</name>
+    <message>
+        <location filename="../../Panel/Panel.ui" line="201"/>
+        <source>Input</source>
+        <translation type="unfinished">输入</translation>
+    </message>
+    <message>
+        <location filename="../../Panel/Panel.ui" line="74"/>
+        <source>Output</source>
+        <translation type="unfinished">输出</translation>
+    </message>
+    <message>
+        <location filename="../../Panel/Panel.ui" line="310"/>
+        <source>Volume mixer</source>
+        <translation type="unfinished">音量混合器</translation>
+    </message>
+    <message>
+        <location filename="../../Panel/Panel.cpp" line="410"/>
+        <source>All quiet for now.</source>
+        <translation type="unfinished">现在一切都安静了。</translation>
+    </message>
+</context>
+<context>
+    <name>QuickSoundSwitcher</name>
+    <message>
+        <location filename="../../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="50"/>
+        <source>Run at startup</source>
+        <translation type="unfinished">跟随 Windows 启动</translation>
+    </message>
+    <message>
+        <location filename="../../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="55"/>
+        <source>Settings</source>
+        <translation type="unfinished">设置</translation>
+    </message>
+    <message>
+        <location filename="../../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="58"/>
+        <source>Exit</source>
+        <translation type="unfinished">退出</translation>
+    </message>
+</context>
+<context>
+    <name>SoundOverlay</name>
+    <message>
+        <location filename="../../SoundOverlay/SoundOverlay.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished">从</translation>
+    </message>
+    <message>
+        <location filename="../../SoundOverlay/SoundOverlay.ui" line="20"/>
+        <source>icon</source>
+        <translation type="unfinished">图标</translation>
+    </message>
+    <message>
+        <location filename="../../SoundOverlay/SoundOverlay.ui" line="46"/>
+        <source>00</source>
+        <translation type="unfinished">00</translation>
+    </message>
+</context>
+</TS>
