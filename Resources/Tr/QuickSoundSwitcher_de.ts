da8889c3091fc3170057dfe735c812657ba88d6f
Steve <51911097+uDEV2019@users.noreply.github.com>
2025-01-04 14:46:00 +0100

    german translation

---
+++ Resources/Tr/QuickSoundSwitcher_de.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>OverlaySettings</name>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="55"/>
+        <source>Mute overlay settings</source>
+        <translation>Overlay-Einstellungen stumm schalten</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="299"/>
+        <source>Overlay position on screen:</source>
+        <translation>Overlay-Position auf dem Bildschirm:</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="42"/>
+        <source>Disable overlay</source>
+        <translation>Overlay deaktivieren</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="109"/>
+        <source>Volume increment percentage</source>
+        <translation>Prozentsatz der Lautstärkeerhöhung</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="281"/>
+        <source>Use static overlay</source>
+        <translation>Statisches Overlay verwenden</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="318"/>
+        <source>Volume control settings</source>
+        <translation>Einstellungen zur Lautstärkeregelung</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="338"/>
+        <source>Disable mute hotkey</source>
+        <translation>Hotkey für Stummschaltung deaktivieren</translation>
+    </message>
+    <message>
+        <location filename="../../OverlaySettings/OverlaySettings.ui" line="73"/>
+        <source>Disable sound notification</source>
+        <translation>Tonbenachrichtigung deaktivieren</translation>
+    </message>
+</context>
+<context>
+    <name>Panel</name>
+    <message>
+        <location filename="../../Panel/Panel.ui" line="201"/>
+        <source>Input</source>
+        <translation>Eingang</translation>
+    </message>
+    <message>
+        <location filename="../../Panel/Panel.ui" line="74"/>
+        <source>Output</source>
+        <translation>Ausgang</translation>
+    </message>
+    <message>
+        <location filename="../../Panel/Panel.ui" line="310"/>
+        <source>Volume mixer</source>
+        <translation>Lautstärkemixer</translation>
+    </message>
+    <message>
+        <location filename="../../Panel/Panel.cpp" line="410"/>
+        <source>All quiet for now.</source>
+        <translation>Im Moment ist alles ruhig.</translation>
+    </message>
+</context>
+<context>
+    <name>QuickSoundSwitcher</name>
+    <message>
+        <location filename="../../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="50"/>
+        <source>Run at startup</source>
+        <translation>Mit Windows starten</translation>
+    </message>
+    <message>
+        <location filename="../../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="55"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="58"/>
+        <source>Exit</source>
+        <translation>Beenden</translation>
+    </message>
+</context>
+<context>
+    <name>SoundOverlay</name>
+    <message>
+        <location filename="../../SoundOverlay/SoundOverlay.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../SoundOverlay/SoundOverlay.ui" line="20"/>
+        <source>icon</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../SoundOverlay/SoundOverlay.ui" line="46"/>
+        <source>00</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
