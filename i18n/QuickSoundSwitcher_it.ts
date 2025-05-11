fb3bdb25a50217b84cf852a4df27e39f29e23dce
bovirus <1262554+bovirus@users.noreply.github.com>
2025-05-11 11:14:30 +0200

    Add Italian language

---
+++ i18n/QuickSoundSwitcher_it.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>QuickSoundSwitcher</name>
+    <message>
+        <location filename="../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="63"/>
+        <source>Use volume mixer only</source>
+        <translation>Usa solo mixer volume</translation>
+    </message>
+    <message>
+        <location filename="../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="68"/>
+        <source>Run at startup</source>
+        <translation>Esegui all'avvio del sistema</translation>
+    </message>
+    <message>
+        <location filename="../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="73"/>
+        <source>Exit</source>
+        <translation>Esci</translation>
+    </message>
+</context>
+<context>
+    <name>SoundPanel</name>
+    <message>
+        <location filename="../Resources/qml/SoundPanel.qml" line="78"/>
+        <source>Audio devices</source>
+        <translation>Dispositivi audio</translation>
+    </message>
+    <message>
+        <location filename="../Resources/qml/SoundPanel.qml" line="223"/>
+        <source>Applications</source>
+        <translation>Applicazioni</translation>
+    </message>
+</context>
+</TS>
