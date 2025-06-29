ce4cc0c724468a59039cefac8dc0f5a8bc85627b
bovirus <1262554+bovirus@users.noreply.github.com>
2025-06-29 19:26:58 +0200

    Italian language update

--- i18n/QuickSoundSwitcher_it.ts
+++ i18n/QuickSoundSwitcher_it.ts
@@ -14,142 +14,142 @@
     <message>
         <location filename="../qml/SettingsWindow.qml" line="17"/>
         <source>QuickSoundSwitcher - Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>QuickSoundSwitcher - Impostazioni</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="67"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Generale</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="71"/>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation>Apparenza</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="75"/>
         <source>Debug</source>
-        <translation type="unfinished"></translation>
+        <translation>Debug</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="154"/>
         <source>General Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Impostazioni generali</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="163"/>
         <source>Panel mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Modo pannello</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="170"/>
         <source>Devices + Mixer</source>
-        <translation type="unfinished"></translation>
+        <translation>Dispositivi + mixer</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="170"/>
         <source>Mixer only</source>
-        <translation type="unfinished"></translation>
+        <translation>Solo mixer</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="170"/>
         <source>Devices only</source>
-        <translation type="unfinished"></translation>
+        <translation>Solo dispositivi</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="180"/>
         <source>Link same input and output devices</source>
-        <translation type="unfinished"></translation>
+        <translation>Collegamento stesso dispositivo ingresso/uscita</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="194"/>
         <source>Sound keepalive</source>
-        <translation type="unfinished"></translation>
+        <translation>Mantieni attivo suono</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="208"/>
         <source>Run at system startup</source>
-        <translation type="unfinished"></translation>
+        <translation>Esegui ad avvio del sistema</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="232"/>
         <source>Appearance &amp; Position</source>
-        <translation type="unfinished"></translation>
+        <translation>Apparenza e posizione</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="241"/>
         <source>Panel position</source>
-        <translation type="unfinished"></translation>
+        <translation>Posizione pannello</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="247"/>
         <source>Top</source>
-        <translation type="unfinished"></translation>
+        <translation>Alto</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="247"/>
         <source>Bottom</source>
-        <translation type="unfinished"></translation>
+        <translation>Basso</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="247"/>
         <source>Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Sinistra</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="247"/>
         <source>Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Destra</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="257"/>
         <source>Taskbar offset</source>
-        <translation type="unfinished"></translation>
+        <translation>Offset barra attività</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="275"/>
         <source>Panel margin</source>
-        <translation type="unfinished"></translation>
+        <translation>Margine pannello</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="293"/>
         <source>Use short device names</source>
-        <translation type="unfinished"></translation>
+        <translation>Usa nome corto dispositivo</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="307"/>
         <source>Volume value display mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Modalità visualizzazione valore volume</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="313"/>
         <source>Slider tooltip</source>
-        <translation type="unfinished"></translation>
+        <translation>Suggerimento slider</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="313"/>
         <source>Label</source>
-        <translation type="unfinished"></translation>
+        <translation>Etichetta</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="313"/>
         <source>Hidden</source>
-        <translation type="unfinished"></translation>
+        <translation>Nascosto</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="332"/>
         <source>Debug and information</source>
-        <translation type="unfinished"></translation>
+        <translation>Debug e informazioni</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="366"/>
         <source>Application version</source>
-        <translation type="unfinished"></translation>
+        <translation>Versione applicazione</translation>
     </message>
     <message>
         <location filename="../qml/SettingsWindow.qml" line="381"/>
         <source>QT version</source>
-        <translation type="unfinished"></translation>
+        <translation>Versione QT</translation>
     </message>
 </context>
 </TS>
