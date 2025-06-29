cc5396e3798f6b2e7e903845fbef3ae0d7a21022
Odizinne <flora.aubry@proton.me>
2025-06-29 10:24:10 +0200

    updated french translation

--- i18n/QuickSoundSwitcher_en.ts
+++ i18n/QuickSoundSwitcher_en.ts
@@ -4,31 +4,151 @@
 <context>
     <name>QuickSoundSwitcher</name>
     <message>
-        <location filename="../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="63"/>
-        <source>Use volume mixer only</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="68"/>
-        <source>Run at startup</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="73"/>
+        <location filename="../src/quicksoundswitcher.cpp" line="117"/>
         <source>Exit</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
-    <name>SoundPanel</name>
+    <name>SettingsWindow</name>
     <message>
-        <location filename="../Resources/qml/SoundPanel.qml" line="78"/>
-        <source>Audio devices</source>
+        <location filename="../qml/SettingsWindow.qml" line="17"/>
+        <source>QuickSoundSwitcher - Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Resources/qml/SoundPanel.qml" line="223"/>
-        <source>Applications</source>
+        <location filename="../qml/SettingsWindow.qml" line="41"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="45"/>
+        <source>Appearance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="49"/>
+        <source>Debug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="128"/>
+        <source>General Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="137"/>
+        <source>Panel mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="144"/>
+        <source>Devices + Mixer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="144"/>
+        <source>Mixer only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="144"/>
+        <source>Devices only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="154"/>
+        <source>Link same input and output devices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="168"/>
+        <source>Sound keepalive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="182"/>
+        <source>Run at system startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="206"/>
+        <source>Appearance &amp; Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="215"/>
+        <source>Panel position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="221"/>
+        <source>Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="221"/>
+        <source>Bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="221"/>
+        <source>Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="221"/>
+        <source>Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="231"/>
+        <source>Taskbar offset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="249"/>
+        <source>Panel margin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="267"/>
+        <source>Use short device names</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="281"/>
+        <source>Volume value display mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="287"/>
+        <source>Slider tooltip</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="287"/>
+        <source>Label</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="287"/>
+        <source>Hidden</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="306"/>
+        <source>Debug and information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="315"/>
+        <source>Application version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="329"/>
+        <source>QT version</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- i18n/QuickSoundSwitcher_fr.ts
+++ i18n/QuickSoundSwitcher_fr.ts
@@ -4,32 +4,152 @@
 <context>
     <name>QuickSoundSwitcher</name>
     <message>
-        <location filename="../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="63"/>
-        <source>Use volume mixer only</source>
-        <translation>Mixeur de volume uniquement</translation>
-    </message>
-    <message>
-        <location filename="../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="68"/>
-        <source>Run at startup</source>
-        <translation>Lancer au démarrage</translation>
-    </message>
-    <message>
-        <location filename="../QuickSoundSwitcher/QuickSoundSwitcher.cpp" line="73"/>
+        <location filename="../src/quicksoundswitcher.cpp" line="117"/>
         <source>Exit</source>
         <translation>Quitter</translation>
     </message>
 </context>
 <context>
-    <name>SoundPanel</name>
+    <name>SettingsWindow</name>
     <message>
-        <location filename="../Resources/qml/SoundPanel.qml" line="78"/>
-        <source>Audio devices</source>
-        <translation>Périphériques</translation>
+        <location filename="../qml/SettingsWindow.qml" line="17"/>
+        <source>QuickSoundSwitcher - Settings</source>
+        <translation>QuickSoundSwitcher - Paramètres</translation>
     </message>
     <message>
-        <location filename="../Resources/qml/SoundPanel.qml" line="223"/>
-        <source>Applications</source>
-        <translation>Applications</translation>
+        <location filename="../qml/SettingsWindow.qml" line="41"/>
+        <source>General</source>
+        <translation>Général</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="45"/>
+        <source>Appearance</source>
+        <translation>Apparence</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="49"/>
+        <source>Debug</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="128"/>
+        <source>General Settings</source>
+        <translation>Paramètres genéraux</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="137"/>
+        <source>Panel mode</source>
+        <translation>Mode du panneau</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="144"/>
+        <source>Devices + Mixer</source>
+        <translation>Périphériques + Mixer</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="144"/>
+        <source>Mixer only</source>
+        <translation>Mixer uniquement</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="144"/>
+        <source>Devices only</source>
+        <translation>Périphériques uniquement</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="154"/>
+        <source>Link same input and output devices</source>
+        <translation>Lier les périphériques I/O sinilaires</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="168"/>
+        <source>Sound keepalive</source>
+        <translation>Empecher la mise en veille audio</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="182"/>
+        <source>Run at system startup</source>
+        <translation>Lancer au démarrage</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="206"/>
+        <source>Appearance &amp; Position</source>
+        <translation>Apparence et position</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="215"/>
+        <source>Panel position</source>
+        <translation>Position du panneau</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="221"/>
+        <source>Top</source>
+        <translation>Haut</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="221"/>
+        <source>Bottom</source>
+        <translation>Bas</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="221"/>
+        <source>Left</source>
+        <translation>Gauche</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="221"/>
+        <source>Right</source>
+        <translation>Droite</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="231"/>
+        <source>Taskbar offset</source>
+        <translation>Compensation de la barre des taches</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="249"/>
+        <source>Panel margin</source>
+        <translation>Marge du panneau</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="267"/>
+        <source>Use short device names</source>
+        <translation>Utiliser un nom court pour les périphériques</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="281"/>
+        <source>Volume value display mode</source>
+        <translation>Mode d&apos;affichage du volume</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="287"/>
+        <source>Slider tooltip</source>
+        <translation>Infobulle</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="287"/>
+        <source>Label</source>
+        <translation>Texte</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="287"/>
+        <source>Hidden</source>
+        <translation>Caché</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="306"/>
+        <source>Debug and information</source>
+        <translation>Informations et debug</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="315"/>
+        <source>Application version</source>
+        <translation>Version de l&apos;application</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsWindow.qml" line="329"/>
+        <source>QT version</source>
+        <translation>Version de QT</translation>
     </message>
 </context>
 </TS>
