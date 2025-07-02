052efe7960c36fb312e64182238f394cb9fc5f38
Odizinne <flora.aubry@proton.me>
2025-07-02 07:47:09 +0200

    removed now unused env in main

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,6 @@ bool isAnotherInstanceRunning(const QString& processName)
 
 int main(int argc, char *argv[])
 {
-    qputenv("QT_QUICK_CONTROLS_MATERIAL_VARIANT", "Dense");
     QApplication a(argc, argv);
     a.setQuitOnLastWindowClosed(false);
     a.setOrganizationName("Odizinne");
